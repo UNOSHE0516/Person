19ba059a75331c764509c4f5e55be8368232e6c6
UNOSHE0516 <hiro0516jms@gmail.com>
2024-11-13 20:18:58 +0900

    2024-11-13

--- src/Test.java
+++ src/Test.java
@@ -9,5 +9,18 @@ public class Test {
 		
 		System.out.println(taro.name);
 		System.out.println(taro.age);
+		
+		taro.name = "木村次郎";
+		taro.age = 18;
+		
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		
+		taro.name = "鈴木花子";
+		taro.age = 16;
+		
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		
 	}
 }
